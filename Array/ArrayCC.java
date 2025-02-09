72450943ed58446da500389155a48987ceacb251
Vivek Patil <vivekrp4503@gmail.com>
2025-02-09 19:47:57 +0530

    Pattern question solved

---
+++ Array/ArrayCC.java
@@ -0,0 +1,20 @@
+package Array;
+
+import java.util.Scanner;
+
+public class ArrayCC {
+    
+    public static void main(String[] args) {
+        int marks[] = new int[100];
+        Scanner sc = new Scanner(System.in);
+
+        marks[0] = sc.nextInt();
+        marks[1] = sc.nextInt();
+        marks[2] = sc.nextInt();
+
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+
+    }
+}
